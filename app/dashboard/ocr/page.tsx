1cfad39fc23b5d358ba78323ca9c53a133c80ef4
analog-sombra <analogsombra@gmail.com>
2024-07-20 09:35:27 +0530

    build: excel file read updated

--- app/dashboard/ocr/page.tsx
+++ app/dashboard/ocr/page.tsx
@@ -4,6 +4,8 @@ import Image from "next/image";
 import { SetStateAction, useRef, useState } from "react";
 import { toast } from "react-toastify";
 import { createWorker } from "tesseract.js";
+import Papa from "papaparse";
+import * as XLSX from "xlsx";
 
 const OCRView = () => {
   const [img, setImg] = useState<File | null>(null);
@@ -11,6 +13,15 @@ const OCRView = () => {
   const [isOrc, setIsOcr] = useState<boolean>(false);
   const [id, setId] = useState<string>("");
   const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const [csv, setCsv] = useState<File | null>(null);
+  const csvRef = useRef<HTMLInputElement>(null);
+
+  interface PaymentData {
+    amount: string;
+    uid: string;
+  }
+  const [paymentdata, setPaymentData] = useState<PaymentData[]>([]);
 
   const handleLogoChange = async (
     value: React.ChangeEvent<HTMLInputElement>,
@@ -32,6 +43,25 @@ const OCRView = () => {
     // } else {
     //   toast.error("Image file size must be less then 1 mb", { theme: "light" });
     // }
+  };
+
+  const handleCSVChange = async (
+    value: React.ChangeEvent<HTMLInputElement>,
+    setFun: (value: SetStateAction<File | null>) => void
+  ) => {
+    if (value!.target.files?.length == 0) return;
+
+    if (
+      value!.target.files![0].type.endsWith("/csv") ||
+      value!.target.files![0].type.endsWith("/vnd.ms-excel")
+    ) {
+      // convert image to grayscale
+      const file = value!.target.files![0];
+
+      setFun((val) => file);
+    } else {
+      toast.error("Please select an image file.", { theme: "light" });
+    }
   };
 
   const startOCR = async () => {
@@ -79,6 +109,96 @@ const OCRView = () => {
     setIsOcr(false);
     setId("");
     setImg(null);
+  };
+
+  const startCSV = async () => {
+    setIsLoading(true);
+    if (csv == null) {
+      toast.error("Select an csv to continue");
+      setIsLoading(false);
+      return;
+    }
+
+    console.log(csv);
+    let csvFile: File = csv;
+
+    if (csvFile.type.endsWith("/csv")) {
+      // Read the CSV file
+      const reader = new FileReader();
+      reader.onload = () => {
+        const text = reader.result as string;
+        Papa.parse(text, {
+          header: true,
+          complete: (results) => {
+            // console.log(results.data);
+            // Process the CSV data as needed
+
+            let recatoredata = results.data.map((value: any) => {
+              const mydata: PaymentData = {
+                amount: value[" Amount"],
+                uid: value[" UTR ID"],
+              };
+
+              return mydata;
+            });
+            setPaymentData(recatoredata);
+
+            setIsLoading(false);
+          },
+          error: (error: any) => {
+            console.error(error.message);
+            toast.error("Error parsing CSV file");
+            setIsLoading(false);
+          },
+        });
+      };
+      reader.onerror = (error: any) => {
+        console.error(error.message);
+        toast.error("Error reading CSV file");
+        setIsLoading(false);
+      };
+      reader.readAsText(csvFile);
+    } else {
+      try {
+        const reader = new FileReader();
+        reader.onload = (event: ProgressEvent<FileReader>) => {
+          const data = event.target?.result;
+          if (data) {
+            const workbook = XLSX.read(data, { type: "binary" });
+            const sheetName = workbook.SheetNames[0];
+            const worksheet = workbook.Sheets[sheetName];
+            const jsonData = XLSX.utils.sheet_to_json(worksheet);
+
+            let recatoredata = jsonData
+              .map((value: any) => {
+                if (value.hasOwnProperty("__EMPTY_4")) {
+                  if (value["__EMPTY_4"].startsWith("UPI/")) {
+                    const mydata: PaymentData = {
+                      amount: value["__EMPTY_8"],
+                      uid: value["__EMPTY_4"].toString().split("/")[1],
+                    };
+                    return mydata;
+                  }
+                }
+                return undefined;
+              })
+              .filter((item): item is PaymentData => item !== undefined);
+            console.log(recatoredata);
+
+            setPaymentData(recatoredata);
+            // Do something with jsonData
+          }
+          setIsLoading(false);
+        };
+        reader.readAsBinaryString(csvFile);
+      } catch (error) {
+        console.error("Error reading file:", error);
+        toast.error("Error reading file");
+        setIsLoading(false);
+      }
+    }
+
+    setIsLoading(false);
   };
 
   return (
@@ -154,6 +274,34 @@ const OCRView = () => {
           </div>
           <div className="mt-4"></div>
         </div>
+      </div>
+
+      {/* csv section */}
+      <div className="h-[1px] bg-slate-600 w-full"></div>
+      <div>
+        <button
+          onClick={() => csvRef.current?.click()}
+          className="bg-emerald-500 px-4 py-1 rounded-md text-white text-sm"
+        >
+          {csv ? "Change CSV" : "Upload CSV"}
+        </button>
+        {csv && (
+          <button
+            className="bg-blue-500 px-4 py-1 rounded-md text-white text-sm"
+            onClick={startCSV}
+          >
+            Start CSV
+          </button>
+        )}
+        <div className="hidden">
+          <input
+            type="file"
+            ref={csvRef}
+            accept="application/vnd.ms-excel, text/csv"
+            onChange={(val) => handleCSVChange(val, setCsv)}
+          />
+        </div>
+        <div className="mt-4"></div>
       </div>
     </>
   );
